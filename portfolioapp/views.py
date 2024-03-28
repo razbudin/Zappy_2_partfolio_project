5c6411d312ad3dcf1ce06db7ae17e53dc0d40932
razbudin <razbudin@yandex.ru>
2024-03-28 18:05:21 +0300

    5.12 немного Bootstrap, Дальше много Бутстрап. Думаю это окончательно. Через какое то время можно смело удалять. Получилось не очень хорошо, Но учитывая что первый раз, то очень хорошо )))

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -5,3 +5,7 @@ from .models import Project
 def home(request):
     projects = Project.objects.all()
     return render(request, 'portfolioapp/home.html', {'projects': projects})
+
+
+def detail():
+    return None
